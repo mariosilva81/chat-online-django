71ed4d23a006d78371a601bd88c16040ee81ce5a
mariosilva81 <mariosilva81@icloud.com>
2023-12-27 14:30:00 -0300

    feat: created room and message models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    title = models.CharField(max_length=255)
+    messages = models.ManyToManyField("Message")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return "{}".format(self.title)
+
+
+class Message(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return "{}".format(self.user)
