f0d68f52332c0ba39e0e10133b5e026ecdde7516
mariosilva81 <mariosilva81@icloud.com>
2023-12-27 16:54:51 -0300

    feat: created urls and templates files

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Message
 
-# Create your views here.
+
+def home(request):
+    messages = Message.objects.all()
+
+    return render(
+        request,
+        "index.html",
+        {
+            "messages": messages,
+        },
+    )
