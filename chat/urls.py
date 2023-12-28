a9a17ad1ba0c9c266dd69456ab545c414f504644
mariosilva81 <mariosilva81@icloud.com>
2023-12-28 09:06:39 -0300

    feat: created list messages template and room detail view

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, RoomDetailView
 
 urlpatterns = [
     path("", home, name="home"),
+    path("<pk>", RoomDetailView.as_view(), name="room_detail"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
-from .models import Message
+from .models import Room
+from django.views.generic import DetailView
 
 
 def home(request):
-    messages = Message.objects.all()
+    rooms = Room.objects.all()
 
     return render(
         request,
         "home.html",
         {
-            "messages": messages,
+            "rooms": rooms,
         },
     )
+
+
+class RoomDetailView(DetailView):
+    model = Room
+    template_name = "list-messages.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
