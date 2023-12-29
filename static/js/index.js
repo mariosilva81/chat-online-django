f89a0d521e942d50363a76cbcfc7f8b93bd79755
mariosilva81 <mariosilva81@icloud.com>
2023-12-29 10:09:03 -0300

    feat: created date formatting and active room

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,15 @@
 const chatMessages = document.querySelector(".messages");
 
+const setRoomActive = (roomId) => {
+  document
+    .querySelectorAll(".list-rooms li")
+    .forEach((room) => room.classList.remove("active"));
+  document.querySelector(`#room-${roomId}`).classList.add("active");
+};
+
 const getMessages = async (roomId) => {
   const response = await fetch(`/${roomId}`);
   const html = await response.text();
   chatMessages.innerHTML = html;
+  setRoomActive(roomId);
 };
